ad1a338821e7ba3fcb3a7d1942b4534cb36a297d
Jean-Fidele <jntagengwa@gmail.com>
2026-02-03 00:55:36 +0000

    chore: fix prod api error

--- apps/api/src/staff/staff.module.ts
+++ apps/api/src/staff/staff.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
 import { StaffController } from "./staff.controller";
 import { StaffService } from "./staff.service";
 
 @Module({
+  imports: [AuthModule],
   controllers: [StaffController],
   providers: [StaffService],
   exports: [StaffService],
